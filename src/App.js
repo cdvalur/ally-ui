fcddb0376233a2eaeb98bb57ef53291089dbc4dd
chandra sekhar <38929992+cdvalur@users.noreply.github.com>
2021-05-10 19:49:15 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import OKRCentralPage from './pages/okrcentral';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        OKR Central Info
+      </header>
+      <OKRCentralPage />
+    </div>
+  );
+}
+
+export default App;
